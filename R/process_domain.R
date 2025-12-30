bf8f2d5b834b6630cbd73dea8835e56b9b912f5f
scimllab <jayeshpatel1001@mail.com>
2025-12-30 14:01:00 -0500

    modified

--- R/process_domain.R
+++ R/process_domain.R
@@ -23,9 +23,9 @@ process_domain <- function(file_name,
                            mutate_expr =NULL) {
 
   #df <- read_xpt(file.path(projpath, file_name))
-  if(grepl(".xpt",file_name,ignore.case = TRUE)){df<-lapply(file.path(projpath, file_name),read_xpt)}
-  else if(grepl(".sas7dat",file_name,ignore.case = TRUE)){df<-lapply(file.path(projpath, file_name),read_sas)}
-  else if(grepl(".csv",file_name,ignore.case = TRUE)){df<-lapply(file.path(projpath, file_name),read.csv)}
+  if(grepl(".xpt",file_name,ignore.case = TRUE)){df<-as.data.frame(lapply(file.path(projpath, file_name),read_xpt))}
+  else if(grepl(".sas7dat",file_name,ignore.case = TRUE)){df<--as.data.frame(lapply(file.path(projpath, file_name),read_sas))}
+  else if(grepl(".csv",file_name,ignore.case = TRUE)){df<--as.data.frame(lapply(file.path(projpath, file_name),read.csv))}
 
   if (!is.null(datetime_var)) {
     df <- df %>%
